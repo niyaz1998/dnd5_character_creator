390fdc2c19f3c211dd281da22cd642707217b85c
cepuchino <niyaz1998@yandex.ru>
2022-05-28 23:00:35 +0300

    added injectable

--- lib/app/app_runner.dart
+++ lib/app/app_runner.dart
@@ -1,6 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:gin_app_runner/gin_app_runner.dart';
 
+import '../di/injectable.dart';
+
 class MainAppRunner extends AppRunner {
   MainAppRunner(this.env);
 
@@ -10,6 +12,6 @@ class MainAppRunner extends AppRunner {
   Future<void> preloadData() async {
     super.preloadData();
     await EasyLocalization.ensureInitialized();
-    // configureDependencies(env);
+    configureDependencies(env);
   }
 }
---
+++ lib/di/injectable.dart
@@ -0,0 +1,10 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+import 'injectable.config.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit()
+void configureDependencies(String environment) =>
+    $initGetIt(getIt, environment: environment);
