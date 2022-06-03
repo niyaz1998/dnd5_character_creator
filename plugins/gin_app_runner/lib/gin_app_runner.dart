9d1fcfdf5f93003f0f3d5914338bfb5181fe41c3
cepuchino <niyaz1998@yandex.ru>
2022-06-03 21:12:51 +0300

    refactored

--- plugins/gin_app_runner/lib/gin_app_runner.dart
+++ plugins/gin_app_runner/lib/gin_app_runner.dart
@@ -5,8 +5,8 @@ import 'dart:async';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
-part 'app_builder.dart';
-part 'app_runner.dart';
+part 'app_builder_base.dart';
+part 'app_runner_base.dart';
 part 'device_preview_builder.dart';
 part 'material_app_builder.dart';
 part 'material_app_builder_config.dart';
