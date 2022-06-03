691495e0d1a006c50433d765ccf7fd368c8c340f
aygiz <factum8181@yandex.ru>
2022-06-03 22:34:41 +0300

    Добавил локализацию

---
+++ lib/l10n/generated/app_localizations_ru.dart
@@ -0,0 +1,12 @@
+
+
+
+import 'app_localizations.dart';
+
+/// The translations for Russian (`ru`).
+class AppLocalizationsRu extends AppLocalizations {
+  AppLocalizationsRu([String locale = 'ru']) : super(locale);
+
+  @override
+  String get helloWorld => 'Hello World!';
+}
