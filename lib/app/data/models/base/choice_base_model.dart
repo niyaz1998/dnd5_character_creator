735858303ed29684f7ce310353be469d5122c526
cepuchino <niyaz1998@yandex.ru>
2022-06-04 22:16:57 +0300

    immutables

--- lib/app/data/models/base/choice_base_model.dart
+++ lib/app/data/models/base/choice_base_model.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 import 'reference_base_model.dart';
@@ -7,6 +8,7 @@ part 'choice_base_model.g.dart';
 /// базовая модель dnd5e API
 /// представляет собой выбор из каких то сущностей
 @JsonSerializable()
+@immutable
 class ChoiceBaseModel {
   final int choose;
   final String type;
--- lib/app/data/models/base/reference_base_model.dart
+++ lib/app/data/models/base/reference_base_model.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'reference_base_model.g.dart';
 
 /// базовая сущность всего dnd 5e API
 @JsonSerializable()
+@immutable
 class ReferenceBaseModel {
   /// идентификатор в dnd5eAPI
   final String index;
