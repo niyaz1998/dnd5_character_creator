7d7850fc52890e04814380c67febb7bc6ce0874f
cepuchino <niyaz1998@yandex.ru>
2022-06-11 16:51:03 +0300

    comment

--- lib/app/data/models/reference_model_converter.dart
+++ lib/app/data/models/reference_model_converter.dart
@@ -27,6 +27,8 @@ class ResourceListModelConverter<T extends ReferenceBaseModel>
         return ProficiencyModel.fromJson(json) as T;
       case SkillModel:
         return SkillModel.fromJson(json) as T;
+
+      /// должно быть последним
       case ReferenceBaseModel:
         return ReferenceBaseModel.fromJson(json) as T;
     }
