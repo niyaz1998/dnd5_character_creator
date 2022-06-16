8f35b7398354ab10c4a32a205d355268377a182c
cepuchino <niyaz1998@yandex.ru>
2022-06-16 16:25:17 +0300

    init for json parser

---
+++ plugins/json_united_parser/bin/json_united_parser.dart
@@ -0,0 +1,5 @@
+import 'package:json_united_parser/json_united_parser.dart' as json_united_parser;
+
+void main(List<String> arguments) {
+  print('Hello world: ${json_united_parser.calculate()}!');
+}
---
+++ plugins/json_united_parser/test/json_united_parser_test.dart
@@ -0,0 +1,8 @@
+import 'package:json_united_parser/json_united_parser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
