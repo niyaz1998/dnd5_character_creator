5337cb67c8c63c88f93c2e7341297b4e0987148f
cepuchino <niyaz1998@yandex.ru>
2022-06-17 11:08:54 +0300

    races entities

---
+++ lib/features/library_races/domain/entities/race/race_entity.dart
@@ -0,0 +1,34 @@
+import '../../../../library/domain/entities/base/choice_base_entity.dart';
+import '../../../../library/domain/entities/base/reference_base_entity.dart';
+import '../../../../library_character_data/domain/entity/language_entity.dart';
+import '../../../../library_character_data/domain/entity/proficiency_entity.dart';
+import 'race_base_interface.dart';
+
+class RaceEntity extends RaceBaseInterface {
+  const RaceEntity({
+    required super.index,
+    required super.name,
+    required super.abilityBonuses,
+    required this.speed,
+    required this.alignment,
+    required this.age,
+    required this.size,
+    required this.sizeDescription,
+    required super.startingProficiencies,
+    required this.startingProficiencyOptions,
+    required super.languages,
+    required this.languageDesc,
+    required this.traits,
+    required this.subRaces,
+  });
+
+  final double speed;
+  final String alignment;
+  final String age;
+  final String size;
+  final String sizeDescription;
+  final ChoiceBaseEntity<ProficiencyEntity> startingProficiencyOptions;
+  final String languageDesc;
+  final List<ReferenceBaseEntity> traits;
+  final List<ReferenceBaseEntity> subRaces;
+}
