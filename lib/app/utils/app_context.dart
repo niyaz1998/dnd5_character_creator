3bc4955a53073572812c859cc70c69e3a1ca5276
aygiz <factum8181@yandex.ru>
2022-06-21 21:28:59 +0300

    Рефакторинг

--- lib/app/utils/app_context.dart
+++ lib/app/utils/app_context.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 
 import '../../l10n/generated/app_localizations.dart';
 
-/// use locale
+/// [appLocale] use locale
 /// example:
 /// final locale = context.appLocale
 /// Text(locale.dnd)
